836905b5769162c141c53ab5d31f5eaadec0093d
Leon Tan <101375873+itsLeonTan@users.noreply.github.com>
2026-04-15 20:53:25 -0700

    v2.5.1 - js for nav menu

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
+let menuOpen = false;
 function menuFunction(x) {
+    menuOpen = !menuOpen;
     x.classList.toggle("change");
+
+    if (menuOpen) {
+        document.querySelector("nav").style.height = "100vh";
+        document.getElementById("nav-bottom").style.display = "grid";
+    }
+    else {
+        document.querySelector("nav").style.height = "fit-content";
+        document.getElementById("nav-bottom").style.display = "none";
+    }
+    
 }
